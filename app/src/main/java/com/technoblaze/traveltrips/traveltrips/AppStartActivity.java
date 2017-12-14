49453b393d4a716b297ee3456bb999e3414e878e
Unknown <prasanth1308@gmail.com>
2017-12-14 09:16:05 +0530

    Onboarding

---
+++ app/src/main/java/com/technoblaze/traveltrips/traveltrips/AppStartActivity.java
@@ -0,0 +1,69 @@
+package com.technoblaze.traveltrips.traveltrips;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
+
+public class AppStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the shared preferences
+        SharedPreferences preferences =  getSharedPreferences("my_preferences", MODE_PRIVATE);
+
+        // Check if onboarding_complete is false
+        if(!preferences.getBoolean("onboarding_complete",false)) {
+            // Start the onboarding Activity
+            Intent onboarding = new Intent(this, OnBoarding.class);
+            startActivity(onboarding);
+
+            // Close the main Activity
+            finish();
+            Toast.makeText(this, "HIT", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        checkForUpdates();
+        //setContentView(R.layout.activity_app_start);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // ... your own onResume implementation
+        checkForCrashes();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterManagers();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterManagers();
+    }
+
+    private void checkForCrashes() {
+        CrashManager.register(this);
+    }
+
+    private void checkForUpdates() {
+        // Remove this for store builds!
+        UpdateManager.register(this);
+    }
+
+    private void unregisterManagers() {
+        UpdateManager.unregister();
+    }
+
+}
---
+++ app/src/main/java/com/technoblaze/traveltrips/traveltrips/OnBoarding.java
@@ -0,0 +1,96 @@
+package com.technoblaze.traveltrips.traveltrips;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
+import com.codemybrainsout.onboarder.AhoyOnboarderCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OnBoarding extends AhoyOnboarderActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AhoyOnboarderCard ahoyOnboarderCard1 = new AhoyOnboarderCard("Scan Barcode", "Label your packages with a barcode before we collect it from you.", R.drawable.barcode);
+        AhoyOnboarderCard ahoyOnboarderCard2 = new AhoyOnboarderCard("Shipping", "Our huge network of shipping partners ensures that your packages are always on schedule.", R.drawable.truck);
+        AhoyOnboarderCard ahoyOnboarderCard3 = new AhoyOnboarderCard("Payment", "Receive payments immediately after the package is delivered.", R.drawable.wallet);
+
+        ahoyOnboarderCard1.setBackgroundColor(R.color.white);
+        ahoyOnboarderCard2.setBackgroundColor(R.color.white);
+        ahoyOnboarderCard3.setBackgroundColor(R.color.white);
+
+        List<AhoyOnboarderCard> pages = new ArrayList<>();
+
+        pages.add(ahoyOnboarderCard1);
+        pages.add(ahoyOnboarderCard2);
+        pages.add(ahoyOnboarderCard3);
+
+        for (AhoyOnboarderCard page : pages) {
+            page.setTitleColor(R.color.colorOnBoardTitle);
+            page.setDescriptionColor(R.color.colorOnBoardDescription);
+        }
+
+        //Set finish button text
+        setFinishButtonTitle("Get Started");
+
+        //Show/Hide navigation controls
+        showNavigationControls(false);
+
+        //Set pager indicator colors
+        setInactiveIndicatorColor(R.color.InActiveIndicatorColor);
+        setActiveIndicatorColor(R.color.activeIndicatorColor);
+
+        //Set the finish button style
+        setFinishButtonDrawableStyle(ContextCompat.getDrawable(this, R.drawable.rounded_button));
+
+        List<Integer> colorList = new ArrayList<>();
+        colorList.add(R.color.white);
+        colorList.add(R.color.white);
+        colorList.add(R.color.white);
+
+        setColorBackground(colorList);
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+        setFont(face);
+
+        setOnboardPages(pages);
+    }
+
+    @Override
+    public void onFinishButtonPressed() {
+        finishOnboarding();
+        Toast.makeText(this, "Finish Pressed", Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(OnBoarding.this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    private void finishOnboarding() {
+        // Get the shared preferences
+        SharedPreferences preferences =
+                getSharedPreferences("my_preferences", MODE_PRIVATE);
+
+        // Set onboarding_complete to true
+        preferences.edit()
+                .putBoolean("onboarding_complete",true).apply();
+
+        // Launch the main Activity, called MainActivity
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+
+        // Close the OnboardingActivity
+        finish();
+    }
+
+}
